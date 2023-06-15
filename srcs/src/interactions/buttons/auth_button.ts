a62a4c057e0ca72f371441ac6923a2f9463b5b27
Reyan Carlier <reyan.carlier@gmail.com>
2023-06-15 11:16:15 +0200

    Removed conditions on Auth button.

--- srcs/src/interactions/buttons/auth_button.ts
+++ srcs/src/interactions/buttons/auth_button.ts
@@ -7,14 +7,14 @@ import { initAuth } from "../../auth/auth_manager";
 class AuthButton {
     @ButtonComponent({ id: "auth" })
     handler(interaction: ButtonInteraction): void {
-        if(interaction.member && interaction.member.user) {
+        //if(interaction.member && interaction.member.user) {
             const embed = new EmbedBuilder();
             const url = initAuth(interaction.member.user.id);
             embed
                 .setTitle("Authentifie toi !")
                 .setDescription(`Clique [ici](${url}) pour être redirigé sur la page d'authentification de 42.`)
             interaction.reply({ embeds: [embed], ephemeral: true });
-        }
+        //}
     }
 }
 
