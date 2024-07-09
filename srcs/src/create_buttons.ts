211b2cc0ff4d1dc7043281fa5cfc4b29124b6b83
Reyan Carlier <48443830+ReyanCarlier@users.noreply.github.com>
2024-07-09 23:33:00 +0200

    Update create_buttons.ts

--- srcs/src/create_buttons.ts
+++ srcs/src/create_buttons.ts
@@ -5,25 +5,29 @@ import {
 	EmbedBuilder,
 	MessageActionRowComponentBuilder,
 	TextChannel,
-	NewsChannel
+	NewsChannel,
+	ChannelType
 } from "discord.js";
 import { Client } from "discordx";
 import { button_channel_id } from "./config.json";
 
 export async function create_buttons(client: Client) {
 	const channel = await client.channels.fetch(button_channel_id);
-	if (channel) {
-		const messages = await channel.messages.fetch({ limit: 1 });
+
+	if (channel && (channel.type === ChannelType.GuildText || channel.type === ChannelType.GuildNews)) {
+		const textChannel = channel as TextChannel | NewsChannel; // Assurez-vous du type ici
+
+		const messages = await textChannel.messages.fetch({ limit: 1 });
 		if (messages.size != 0) {
 			return;
 		}
-		
+
 		const embed = new EmbedBuilder()
 			.setTitle("Authentification")
 			.setDescription(
 				"Clique sur le bouton ci-dessous pour te connecter avec ton compte 42 et profiter pleinement de ce Discord !"
 			)
-			.setFooter({text: "Originally created for LLD, by shocquen, dhubleur and tbrowang with <3"});
+			.setFooter({ text: "Originally created for LLD, by shocquen, dhubleur and tbrowang with <3" });
 
 		const btn = new ButtonBuilder()
 			.setLabel("Authentification")
@@ -35,7 +39,7 @@ export async function create_buttons(client: Client) {
 				btn
 			);
 
-		await channel.send({ embeds: [embed], components: [buttonRow] });
+		await textChannel.send({ embeds: [embed], components: [buttonRow] });
 	} else {
 		console.log(
 			"Impossible to init auth button because channel is not valid"
